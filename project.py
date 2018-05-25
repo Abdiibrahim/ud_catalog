2fabd0c6264315c1c07fc87383c3534f4d7126ba
Abdi Ibrahim <abdirisak.ibrahim@uoit.net>
2018-05-24 22:30:31 -0400

    Update project.py

--- project.py
+++ project.py
@@ -361,7 +361,7 @@ def categoryJSON():
 	return jsonify(categories=[r.serialize for r in categories])
 
 
-# Initialize on port 5000
+# Initialize on port
 if __name__ == '__main__':
 	app.secret_key = 'super_secret_key'
-	app.run(host='0.0.0.0', port=5000, debug=True)
+	app.run(host='0.0.0.0', port=80, debug=True)
